c9a9e70a7f1710ab47884ad2ed9c4243f21f88fc
MohammadReza Kootvaal <info@famad.ir>
2017-06-23 17:01:00 +0430

    Add SCSS and CSS Files

--- famad-input-counter.js
+++ famad-input-counter.js
@@ -4,13 +4,15 @@ $(function(){
 	///////////////////// Input Character Counter /////////////
     $.fn.famadInputCounter = function(option){
 		var DEFAULTS = {
-			element: '.famad-counter',
-			elementText: '.famad-counter-text',
-			elementInput: '.famad-input-text',
-			textMax: 150
+			element: '.famad-input-counter',
+			elementText: '.famad-input-counter-text',
+			elementInput: '.famad-input-counter-input',
+			textMax: 170,
+			bestRange: 150,
+			warningRange: 160
 		};
         this.each(function() {
-			var setofObjects = ['element','elementText','elementInput','textMax'];
+			var setofObjects = ['element','elementText','elementInput','textMax','bestRange','warningRange'];
 			var $options = {};
 			var $this = $(this);
 			if(option){
@@ -25,8 +27,8 @@ $(function(){
 				}
 			}
 			
-			var bestRange = $options.textMax * 0.7142;
-			var warningRange = $options.textMax * 0.8571;
+			var bestRangePercent = (($options.bestRange * 100)/ $options.textMax);
+			var warningRangePercent = (($options.warningRange * 100)/ $options.textMax);
 			$($this.find($options.elementInput)).attr('maxlength',$options.textMax);
 			$(document).ready(function(){
 				if($($this.find($options.elementInput)).val().length !== null || $($this.find($options.elementInput)).val().length !== 0){
@@ -34,39 +36,39 @@ $(function(){
 					var text_remaining = $options.textMax - text_length;
 					var text_percent = parseInt((text_remaining * 100) / $options.textMax);
 					var percent = 100 - text_percent;
-					if(percent >= 0 && percent < bestRange){
+					if(percent >= 0 && percent < bestRangePercent){
 					 $($this.find($options.element)).addClass('progress-bar-success');
 					}
-					if(percent >= bestRange && percent < warningRange){
+					if(percent >= bestRangePercent && percent < warningRangePercent){
 					 $($this.find($options.element)).addClass('progress-bar-warning');
 					}
-					if(percent >= warningRange && percent < 100){
+					if(percent >= warningRangePercent && percent < 100){
 					 $($this.find($options.element)).addClass('progress-bar-danger');
 					}
 					$($this.find($options.element)).attr("aria-valuenow","10").css("width",percent+"%");
-					$($this.find($options.element)).html(text_remaining + ' remaining');
+					$($this.find($options.elementText)).html(text_remaining + ' remaining');
 				}else{
-					$($this.find($options.element)).html($options.textMax + ' remaining');
+					$($this.find($options.elementText)).html($options.textMax + ' remaining');
 				}
 			});
 			$($this.find($options.elementInput)).on('keyup',function() {
-				$($this.find($options.element)).html($options.textMax + ' remaining');
+				$($this.find($options.elementText)).html($options.textMax + ' remaining');
 				var text_length = $($this.find($options.elementInput)).val().length;
 				var text_remaining = $options.textMax - text_length;
 				var text_percent = parseInt((text_remaining * 100) / $options.textMax);
 				var percent = 100 - text_percent;
 				$($this.find($options.element)).removeClass('progress-bar-success').removeClass('progress-bar-warning').removeClass('progress-bar-danger');
-				if(percent >= 0 && percent < bestRange){
+				if(percent >= 0 && percent < bestRangePercent){
 				 $($this.find($options.element)).addClass('progress-bar-success');
 				}
-				if(percent >= bestRange && percent < warningRange){
+				if(percent >= bestRangePercent && percent < warningRangePercent){
 				 $($this.find($options.element)).addClass('progress-bar-warning');
 				}
-				if(percent >= warningRange && percent < 100){
+				if(percent >= warningRangePercent && percent < 100){
 				 $($this.find($options.element)).addClass('progress-bar-danger');
 				}
 				$($this.find($options.element)).attr("aria-valuenow","10").css("width",percent+"%");
-				$($this.find($options.element)).html(text_remaining + ' remaining');
+				$($this.find($options.elementText)).html(text_remaining + ' remaining');
 			});
 
         });
